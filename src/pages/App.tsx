5df8324f8b086298b68e2a9f2cd4dd733dcd50b2
MarcosAparicio <marcos.aparicio593@gmail.com>
2023-11-05 14:30:07 -0500

    setting project structure

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,23 +1,32 @@
+import {
+  AppBar,
+  Box,
+  Container,
+  CssBaseline,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import { ThemeProvider } from "@mui/system";
 import React from "react";
+import { theme } from "../theme/theme";
+import { TheForm } from "./Form";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <ThemeProvider theme={theme}>
+        <AppBar position="fixed">
+          <Toolbar variant="dense">
+            <Typography variant="h6">Multi-Step Form</Typography>
+          </Toolbar>
+        </AppBar>
+        <CssBaseline />
+        <Container>
+          <Box mt={10}>
+            <TheForm />
+          </Box>
+        </Container>
+      </ThemeProvider>
+    </>
   );
 }
-
-export default App;
