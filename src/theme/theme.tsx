247a7f842d6334d4c1eb7d6aaa2feb0ddad96b47
MarcosAparicio <marcos.aparicio593@gmail.com>
2024-01-03 13:42:29 -0500

    adding some overridden props for the form components

--- src/theme/theme.tsx
+++ src/theme/theme.tsx
@@ -3,7 +3,11 @@ import { createTheme } from "@mui/material/styles";
 export const useCustomTheme = (mode: "light" | "dark") => {
   return createTheme({
     palette: {
-      mode,
+      mode: mode,
+    },
+    components: {
+      MuiTextField: { defaultProps: { fullWidth: true } },
+      MuiButtonBase: { defaultProps: { disableRipple: true } },
     },
   });
 };
